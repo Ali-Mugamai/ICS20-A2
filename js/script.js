1a935e0af6c37abee9b83a309534b3307ab9543b
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2022-10-21 02:06:07 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -15,5 +15,6 @@ function calculate() {
   // process
   const area = (length * height) / 2
   // output
-  document.getElementById("area_of_rhombus").innerHTML = "Area is: " + area + " cm²"
+  document.getElementById("area_of_rhombus").innerHTML =
+    "Area is: " + area + " cm²"
 }
