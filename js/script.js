05b4b2c873c566e6f64ab7ceb303fd6c25ca7521
Ali-Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-10-20 22:06:38 -0400

    bruuhhh

--- js/script.js
+++ js/script.js
@@ -15,6 +15,5 @@ function calculate() {
   // process
   const area = (length * height) / 2
   // output
-  document.getElementById("area_of_rhombus").innerHTML =
-    "Area is: " + area + " cm²"
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
 }
